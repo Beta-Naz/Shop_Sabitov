b98be0aa56fcf7c4d6f8dc93182e18646d524c1b
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-14 11:43:16 +0500

    Сделана системная папка. Сабитов

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-using Microsoft.AspNetCore.Builder;
+using Shop.Data.Interfaces;
+using Shop.Data.Mocks;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<ICategory, MockCaregory>();
+builder.Services.AddTransient<IItem, MockItem>();
+
 builder.Services.AddMvc(op => op.EnableEndpointRouting = false);
 var app = builder.Build();
 

1a783f9aec7c6e319897932a34f5ab318d00ce15
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-14 10:51:52 +0500

    Разработанные модели данных. Сабитов

---
+++ Data/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.Data.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public int Price { get; set; }
+        public Category Category { get; set; }
+    }
+}

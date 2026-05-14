2ba62c2f4356097b340d5853f2e19eb6b0e58bf1
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-14 11:09:23 +0500

    Разработанный контроллер предметов. Сабитов

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+
+
+namespace Shop.Controllers
+{
+    public class ItemController : Controller
+    {
+        private IItem IAllItems;
+        private ICategory IAllCategories;
+        public ItemController(IItem iAllItems, ICategory iAllCategories)
+        {
+            IAllItems = iAllItems;
+            IAllCategories = iAllCategories;
+        }
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница с предметами";
+            var cars = IAllItems.AllItems;
+            return View(cars);
+        }
+    }
+}

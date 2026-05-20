2c7637f55d17238ad0c78292b6eef22d027bb06f
Misha <axxuetam@gmail.com>
2026-05-20 09:55:53 +0500

    На отлично. Сабитов

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+
+namespace Shop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ICategory IAllCategories;
+        public CategoryController(ICategory iAllCategories)
+        {
+            IAllCategories = iAllCategories;
+        }
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница категории";
+            var cars = IAllCategories.AllCategories;
+            return View(cars);
+        }
+    }
+}

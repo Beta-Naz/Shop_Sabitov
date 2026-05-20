5bff364d4fd1b7f6581899c9dd7bcb47115f548d
Beta-Naz <sabitovnazir6@gmail.com>
2026-05-20 21:37:08 +0500

    Получение данных о предметах. Сабитов

---
+++ Data/DataBase/DBItems.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using Shop.Data.Common;
+using Shop.Data.Interfaces;
+using Shop.Data.Models;
+namespace Shop.Data.DataBase
+{
+    public class DBItems : IItem
+    {
+        public IEnumerable<Category> Categories = new DBCategory().AllCategories;
+
+        public IEnumerable<Item> AllItems
+        {
+            get
+            {
+                List<Item> items = new List<Item>();
+                MySqlConnection MySqlConnection = Connection.CreateConnection();
+                MySqlDataReader ItemsData = Connection.Query("SELECT * FROM shop.items ORDER BY `Name`;", MySqlConnection);
+                while (ItemsData.Read())
+                {
+                    items.Add(new Item()
+                    {
+                        Id = ItemsData.IsDBNull(0) ? -1 : ItemsData.GetInt32(0),
+                        Name = ItemsData.IsDBNull(1) ? "" : ItemsData.GetString(1),
+                        Description = ItemsData.IsDBNull(2) ? "" : ItemsData.GetString(2),
+                        Img = ItemsData.IsDBNull(3) ? "" : ItemsData.GetString(3),
+                        Price = ItemsData.IsDBNull(4) ? -1 : ItemsData.GetInt32(4),
+                        Category = ItemsData.IsDBNull(5) ? null : Categories.Where(x => x.Id == ItemsData.GetInt32(5)).First()
+                    });
+                }
+                MySqlConnection.Close();
+                return items;
+            }
+        }
+    }
+}

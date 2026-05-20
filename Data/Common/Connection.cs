509909117260742a77973f9da1cfdae1074426f4
Beta-Naz <sabitovnazir6@gmail.com>
2026-05-20 19:41:22 +0500

    Разработанное подключение к MySql. Сабитов

---
+++ Data/Common/Connection.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Shop.Data.Common
+{
+    public class Connection
+    {
+        private readonly static string _connectionData =
+            @"server=127.0.0.1;
+              database=shop
+              uid=root
+              pwd=1234";
+        public static MySqlConnection CreateConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(_connectionData);
+            connection.Open();
+            return connection;
+        }
+        public static MySqlDataReader Query(string sql, MySqlConnection connection)
+        {
+            return new MySqlCommand(sql, connection).ExecuteReader();
+        }
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
